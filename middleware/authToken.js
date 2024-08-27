985619036bf1e7a15622c82b6c2621063873b011
harshs.relytree <harshs.relytree@gmail.com>
2024-08-27 11:29:25 +0530

    changes

--- middleware/authToken.js
+++ middleware/authToken.js
@@ -3,6 +3,8 @@ const jwt = require('jsonwebtoken')
 async function authToken(req,res,next){
     try{
         const token = req.cookies?.token
+        console.log("token",token);
+        
 
         if(!token){
             return res.status(200).json({
